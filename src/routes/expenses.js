0f109a8a8653d2e6fb413ec016afdce12d929337
alvarollmenezes <alvarollmenezes@gmail.com>
2016-11-08 14:29:39 -0200

    Adiciona cache nas rotas

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -1,10 +1,12 @@
+const apicache = require( 'apicache' ).options( { debug: false } ).middleware;
+
 module.exports = app => {
 
     const expensesController = require( '../controllers/expensesController' )();
 
-    app.get( '/expenses/area', expensesController.byArea );
-    app.get( '/expenses/origin', expensesController.byOrigin );
-    app.get( '/expenses/detail/:id', expensesController.detail );
+    app.get( '/expenses/area', apicache( '1 hour' ), expensesController.byArea );
+    app.get( '/expenses/origin', apicache( '1 hour' ), expensesController.byOrigin );
+    app.get( '/expenses/detail/:id', apicache( '1 hour' ), expensesController.detail );
 
     return app;
 };
